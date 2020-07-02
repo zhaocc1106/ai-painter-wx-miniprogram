b87af0714ae6505979af8296bfe50169b27cfbdb
zhaochaochao <zhaochaochao@baidu.com>
2020-07-02 17:52:00 +0800

    Fix share bug.

--- pages/share/share.js
+++ pages/share/share.js
@@ -21,91 +21,12 @@ Page({
         views: [
           {
             type: 'image',
-            url: 'https://hybrid.xiaoying.tv/miniprogram/viva-ad/1/1531103986231.jpeg',
-            top: 0,
-            left: 0,
-            width: 375,
-            height: 555
-          },
-          {
-            type: 'image',
-            url: 'https://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83epJEPdPqQVgv6D8bojGT4DrGXuEC4Oe0GXs5sMsN4GGpCegTUsBgL9SPJkN9UqC1s0iakjQpwd4h4A/132',
-            top: 27.5,
-            left: 29,
-            width: 55,
-            height: 55
-          },
-          {
-            type: 'image',
-            url: 'https://hybrid.xiaoying.tv/miniprogram/viva-ad/1/1531401349117.jpeg',
-            top: 27.5,
-            left: 29,
-            width: 55,
-            height: 55
-          },
-          {
-            type: 'text',
-            content: '您的好友【kuckboy】',
-            fontSize: 16,
-            color: '#402D16',
-            textAlign: 'left',
-            top: 33,
-            left: 96,
-            bolder: true
-          },
-          {
-            type: 'text',
-            content: '创作了一幅世界名画，邀请你一起欣赏！',
-            fontSize: 15,
-            color: '#563D20',
-            textAlign: 'left',
-            top: 59.5,
-            left: 96
-          },
-          {
-            type: 'image',
             url: imgUrl,
             top: 136,
             left: 42.5,
             width: 290,
             height: 240
           },
-          {
-            type: 'image',
-            url: 'https://www.geekxz.com/huihuaqu.jpg',
-            top: 443,
-            left: 85,
-            width: 68,
-            height: 68
-          },
-          {
-            type: 'text',
-            content: '这就是标题-蒙娜丽莎的微笑',
-            fontSize: 16,
-            lineHeight: 21,
-            color: '#383549',
-            textAlign: 'left',
-            top: 390,
-            left: 44,
-            width: 287,
-            MaxLineNumber: 2,
-            breakWord: true,
-            bolder: true
-          },
-          
-          {
-            type: 'text',
-            content: '长按识别图中二维码和我一起来绘画~',
-            fontSize: 14,
-            color: '#383549',
-            textAlign: 'left',
-            top: 460,
-            left: 165.5,
-            lineHeight: 20,
-            MaxLineNumber: 2,
-            breakWord: true,
-            width: 125
-          }
         ]
       }
     })
@@ -134,8 +55,8 @@ Page({
    */
   onShareAppMessage: function () {
     return {
-      title: '来猜猜我画的是什么',
-      path: `/pages/games/draw-guess/guess-pic/index?id=${this.data.levelId}`,
+      title: '我创作了一副名画，你也来试一下吧!',
+      path: `/pages/ai-painter/index`,
       imageUrl: this.data.imgUrl,
       success: function (res) {
         // 转发成功
